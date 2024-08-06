cc102e84530a1716ff3bee1536d51a5221b71e87
Michael Cooper <coopermj@cs.toronto.edu>
2024-08-06 11:41:59 -0400

    Update mainpage w/ registration link, corrected date/time info, and shorter writing

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import React, { useState, useEffect } from 'react';
 import { Mate } from 'next/font/google'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHouse } from '@fortawesome/free-solid-svg-icons'
-import { IconButton } from '@mui/material';
+import { Button, IconButton } from '@mui/material';
 import { faGithubSquare, faLinkedin, faXTwitter } from "@fortawesome/free-brands-svg-icons"
 import Tooltip from '@mui/material/Tooltip';
 
@@ -61,8 +61,10 @@ export default class Home extends React.Component<any, any> {
             <Box width={6/10} p={1}>
               <h1 className="text-xl" style={{paddingBottom: "8px"}}>Red Teaming Large Language Models for Healthcare</h1>
               <p className="text-sm">Workshop at Machine Learning for Healthcare (MLHC), 2024</p>
-              <p className="text-sm">August 16, 2024</p>
-              <p className="text-sm">Arcadian Court, Toronto, ON</p>
+              <p className="text-sm">August 15, 2024, 1:00PM -- 5:00PM</p>
+              <p className="text-sm">27 King's College Cir, Toronto, Ontario</p>
+              <br></br>
+              <Button variant="contained" color="primary" href="https://events.eventzilla.net/e/mlhc-preconference-workshop-2138630204">Register Here</Button>
             </Box>
           </Box>
           ) : (
@@ -77,8 +79,10 @@ export default class Home extends React.Component<any, any> {
               <Box width={8/10} p={1}>
                 <h1 className="text-xl" style={{paddingBottom: "8px"}}>Red Teaming Large Language Models for Healthcare</h1>
                 <p className="text-sm">Workshop at Machine Learning for Healthcare (MLHC), 2024</p>
-                <p className="text-sm">August 16, 2024</p>
-                <p className="text-sm">Arcadian Court, Toronto, ON</p>
+                <p className="text-sm">August 15, 2024, 1:00PM -- 5:00PM</p>
+                <p className="text-sm">27 King's College Cir, Toronto, Ontario</p>
+                <br></br>
+                <Button variant="contained" color="primary" href="https://events.eventzilla.net/e/mlhc-preconference-workshop-2138630204">Register Here</Button>
               </Box>
             </Box>
           )} 
@@ -91,15 +95,15 @@ export default class Home extends React.Component<any, any> {
           <br></br>
 
           <p>
-          Large language models (LLMs) trained on massive data have demonstrated exciting capabilities in information retrieval, decision-making, and text generation. Despite their many strengths, these language models are imperfect, and their deployment in healthcare settings poses risk: language models may hallucinate incorrect responses to factual inquiries, provide inaccurate responses to queries that lie outside of the distribution of training data, or incorrectly apply mathematical formulae (e.g., risk score equations) to clinical data. The goal of this workshop is for teams of clinicians and computer scientists to jointly explore the limitations of the present generation of large language models in realistic healthcare scenarios to inform responsible deployment of LLMs in healthcare and to highlight areas of improvement in the machine learning methods underlying modern LLMs.
+          Large language models (LLMs) trained on massive data have demonstrated exciting capabilities in information retrieval, decision-making, and text generation. Despite their many strengths, these language models are imperfect, and their deployment in healthcare settings poses risk unless adequate safeguards are adopted. The goal of this workshop is for teams of clinicians and computer scientists to jointly explore the limitations of the present generation of large language models in realistic healthcare scenarios.
           </p>
           <br></br>
           <p>
-            This exercise will take the form of a workshop at the Machine Learning for Healthcare Conference (MLHC), 2024. Working in teams of clinicians and computer scientists, workshop participants will undertake a “red teaming” exercise. Red teaming is an established set of techniques within the physical and digital security communities, wherein a group (the “red team”; typically composed of security practitioners) pretends to be a malicious actor and attempts to gain access to the secured location or information. The process of doing so highlights potential limitations of the existing security systems and informs areas of improvement.
+          We hope that insights from this workshop will help to inform responsible deployment of LLMs in healthcare and to highlight areas of improvement in the machine learning methods underlying modern LLMs.
           </p>
           <br></br>
           <p>
-            Within our workshop, participants will place themselves in the shoes of clinical users of LLM-based healthcare systems. Unlike in a traditional red-teaming exercise, where participants pretend to be malicious users, the healthcare context yields the unique restriction that users (clinicians, nurses, hospital administrators) are almost exclusively using the system with the goal of eliciting a useful, positive outcome. In this vein, our “red team” will not play the role of a malicious actor by, say, attempting to elicit racist or sexist behaviour from the LLM that circumvents its system prompt. Rather, our participants will brainstorm realistic clinical scenarios in which LLMs may support information retrieval or decision-making, and highlight the subset of those scenarios in which the limitations of modern LLMs yield harm when prompted by well-intentioned users. For example, an LLM that incorrectly applies a clinical risk score equation to a series of patient biomarkers may lead the user to improperly estimate patient risk – and potentially result in inferior treatment decisions downstream – even though the user prompted the LLM without malicious intent.
+          This workshop will take the form of a hands-on "red teaming" exercise at the Machine Learning for Healthcare Conference (MLHC), 2024. Working in teams of clinicians and computer scientists, workshop participants will brainstorm realistic clinical scenarios in which modern LLMs may support information retrieval or decision-making, and highlight instances in which the model's output may yield downstream harm. For example, an LLM that incorrectly -- but confidently -- states that two drugs do not have harmful interaction properties may result in dangerous downstream treatment decisions, should the model be deployed in a clinical setting.
           </p>
           <br></br>
 
