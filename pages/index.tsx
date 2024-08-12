7b67a24fae128db3c856d327acf83af353c46d20
Michael Cooper <coopermj@cs.toronto.edu>
2024-08-12 14:36:58 -0400

    Add acknowledgements

--- pages/index.tsx
+++ pages/index.tsx
@@ -645,7 +645,16 @@ export default class Home extends React.Component<any, any> {
               
 
           )}
+          <br></br>
+          <div>
+          <p style={{paddingBottom: "10px"}}>We graciously aknowledge the support of the following individuals without whom this workshop would not be possible.</p>
+          <ul>
+            <li style={{paddingLeft: "15px", paddingBottom: "10px"}}><i>Clinical consults:</i> Michael Colacci, M.D., Andre Amarel, M.D., Xun Zhao, M.D., Maxim Ben Yakov, M.D., William Tran, M.D., Robert Grant, M.D., Mamatha Bhat, M.D.</li>
+            <li style={{paddingLeft: "15px", paddingBottom: "10px"}}><i>Vector Institute Guidance & Technical Support:</i> Mark Coatsworth, Amrit Krishnan.</li>
+            <li style={{paddingLeft: "15px", paddingBottom: "10px"}}><i>OpenAI Guidance & Technical Support:</i> Karan Singhal.</li>
+          </ul>
 
+          </div>
 
         </main>
       {/* Pass in whether we are on mobile to the footer */}
