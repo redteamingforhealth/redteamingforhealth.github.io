ba84111ecbab56a80e9bbe5e587720d6b39a5e5f
Michael Cooper <coopermj@cs.toronto.edu>
2024-08-13 12:01:35 -0400

    Push partially completed instructions

--- pages/instructions.tsx
+++ pages/instructions.tsx
@@ -94,9 +94,6 @@ export default class Home extends React.Component<any, any> {
           <br></br>
           <hr style={{width: "70%", margin: "auto"}}/>
           <br></br>
-          <h2 style={{paddingBottom: "8px"}} className="text-xl">WiFi Access</h2>
-
-          <p style={{paddingBottom: "6px"}}>Workshop participants can access WiFi at the University of Toronto using eduroam. For instructions, see <a href="https://eva.eis.utoronto.ca" target='_blank'>this link</a>.</p>
 
           <h2 style={{paddingBottom: "8px"}} className="text-xl">Useful Links</h2>
 
@@ -222,7 +219,51 @@ export default class Home extends React.Component<any, any> {
             </a>
           </Box>
 
+          <h2 style={{paddingBottom: "8px", paddingTop: "8px"}} className="text-xl">Workshop Instructions</h2>
 
+          {/* Ordered list with 8px spacing between paragraphs */}
+          <ol style={{paddingBottom: "8px", listStyleType: "decimal", paddingLeft: "1em"}}>
+            <li className="text-sm" style={{paddingBottom: '6px'}}>Introduce yourselves to your group! Briefly share your name, relevant professional background (clinical training / subspecialty, for clinician participants), and one hobby you enjoy doing in your spare time. As you go around the group, one member from the group should be entering this information into the <a href="https://google.com">Group Intake Form</a>.
+            <br></br>
+            <div style={{paddingTop: '6px'}}>The group intake form asks for a group number; this should be clearly written on the piece of paper on your tables.</div>
+            </li>
+            <li className="text-sm" style={{paddingBottom: '6px'}}>In your teams, sign in to each fo the three language model platforms (GPT-4o, Claude, Open-Source) using the credentials written on the pieces of paper on your tables.</li>
+            <li className="text-sm" style={{paddingBottom: '6px'}}>In your teams, take 10-15 minutes to get acquainted with one or more of the language models. Here are a few suggestions for exercises to try:
+              <ul style={{paddingLeft: '15px'}}>
+                <li style={{listStyleType: 'disc', paddingBottom: '6px'}}>
+                  <p style={{paddingBottom: '6px'}}><b>Ask the model to write you a poem</b> about machine learning in healthcare, in a particular style or tone. E.g.,</p>
+                  <code>
+                    <p style={{paddingBottom: '6px'}}>Write me a poem about machine learning in healthcare in the style of Dr. Seuss.</p>
+                    <p style={{paddingBottom: '6px'}}>Write me a romantic Shakespearean sonnet about machine learning in healthcare.</p>
+                  </code>
+                  The models should be able to do this remarkably well; this highlights that, by learning complex probability distributions over the structure of natural language in their training data, they can generate text that is coherent and stylistically consistent.
+                </li>
+                <li style={{listStyleType: 'disc'}}>
+                  <p style={{paddingBottom: '6px'}}><b>Ask the model what happened in the news</b> yesterday. E.g.,</p>
+                  <code>
+                    <p style={{paddingBottom: '6px'}}>What happened in the news yesterday?</p>
+                  </code>
+                  The models should not be able to tell you this; this is because events from yesterday's news took place after the training data for these models was collected. This highlights that the models are not omniscient and can only generate text based on the patterns they have learned from their training data.
+                </li>
+                <li style={{listStyleType: 'disc'}}>
+                  <p style={{paddingBottom: '6px'}}><b>Ask the model to generate a list</b> of the top 10 causes of death in the United States in 2022. E.g.,</p>
+                  <code>
+                    <p style={{paddingBottom: '6px'}}>List the top 10 causes of death in the United States in 2022.</p>
+                  </code>
+                  Cross-reference the generaetd list with <a href="https://www.cdc.gov/nchs/fastats/leading-causes-of-death.htm">this one from the CDC</a>. The model may not be perfect, but you should notice a strong degree of overlap between the two lists. This highlights that these models can generate text that is factually accurate and up-to-date, as long as the information was present in their training data.
+                </li>
+                <li style={{listStyleType: 'disc'}}>
+                  <p style={{paddingBottom: '6px'}}>The GPT-4o interface allows users to upload images and query them using text. Try uploading this image of Vahid and <b>asking the model to describe the image</b>. You can drag and drop the image from either this website (or from your computer) into the GPT-4o dialogue box, and then provide a textual instruction, e.g.,</p>
+                  <img src="vahid.png" alt="Vahid" style={{width: "150px"}}/>
+                  <code>
+                    <p style={{paddingBottom: '6px'}}>Describe the image.</p>
+                  </code>
+                  GPT-4o should be able to generate a description of the image that is accurate and detailed. This highlights that these models can generate text that is grounded in the visual information present in an image.
+                </li>
+              </ul>
+            </li>
+            <li className="text-sm" style={{paddingBottom: '6px'}}>Ask one of the clinicians in your group to describe their clinical workflow. What types of [knowledge retrieval / text summarization / text generation / decision-making] tasks do they encounter on a daily basis, and how might access to one of these language models help them in their work? Critically consider potential use cases, as a key goal of this exercise is to ground the use of these models in realistic clinical scenarios.</li>
+          </ol>
 
           {/* Make one box on the left, one box on the right, with form links */}
 
